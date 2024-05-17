a4754678041b7d4be20fea2ab5f791dcb20276d9
marc <marc@LEEPE-MARC>
2024-05-17 21:44:41 +0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './components/details/details.component';
+import { DetailsMoviesComponent } from './components/details-movies/details-movies.component';
+import { DetailsReviewsComponent } from './components/details-reviews/details-reviews.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotAuthorizedComponent } from './components/not-authorized/not-authorized.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    DetailsMoviesComponent,
+    DetailsReviewsComponent,
+    FooterComponent,
+    HomeComponent,
+    LoaderComponent,
+    ModalComponent,
+    NavbarComponent,
+    NotAuthorizedComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
